58109c87614ccf1a51b2e3c48102b9065493e902
mkj786 <unknownveer47@gmail.com>
2025-11-13 01:51:09 +0500

    Update project branding and maintainer information

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /*  
 ==============================================
-🚀 JAWAD-MD v3.0.0 — Official Release  
+🚀 JISKANI-MD v3.0.0 — Official Release  
 ==============================================
 💡 Next-Gen WhatsApp MD Bot for 2025  
 🔥 Ultra-Fast | Smart AI | Packed with Features  
 
-📌 Developed & Maintained By: JawadTech  
+📌 Developed & Maintained By: 亗•MOᕼᔕIᑎ🎀Sᕼᗩᕼᘔᗩᗪ✿᭄  
 ❤️ Crafted with Passion, Built for Performance  
 
 ⚠️ Note: Old deployment repository is now closed.  
 ✅ Deploy only from the NEW official repository.  
 
-Powered By → JawadTechX | 2025 🚀
+Powered By → 亗•MOᕼᔕIᑎ🎀Sᕼᗩᕼᘔᗩᗪ✿᭄ | 2025 🚀
 ==============================================
 */
